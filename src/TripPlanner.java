d6004763d56aff1fbb83a857d5bb4f946399c12c
Robert Young <robertyoung@Roberts-MacBook-Pro.local>
2019-01-04 10:10:04 +0000

    Initial commmit

---
+++ src/TripPlanner.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class TripPlanner {
+
+
+    public static void main(String [] args) {}
+
+    public static void intro() {
+        Scanner input = new Scanner(System.in);
+        // takes in user name and destination
+
+    }
+
+    public static void budget() {
+        Scanner input = new Scanner(System.in);
+        // takes in the days planned on the trip, allowance, and conversion info
+
+    }
+
+    public static void time() {
+        Scanner input = new Scanner(System.in);
+        // takes in the time difference between home and destination
+    }
+
+
+    public static void distance() {
+        Scanner input = new Scanner(System.in);
+        // takes in distance between home and destination in square kilometeres
+        // converts into square mile
+
+        // kilometers*0.62137 = miles
+    }
+}
