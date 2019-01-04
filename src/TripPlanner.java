d34f8d96815d2eadb206598b78bfc1d3ef078d67
Robert Young <robertyoung@Roberts-MacBook-Pro.local>
2019-01-04 11:37:18 +0000

    Budget method complete

--- src/TripPlanner.java
+++ src/TripPlanner.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
-
+import java.lang.Math;
 
 public class TripPlanner {
 
 
+
     public static void main(String [] args) {
         intro();
+        budget();
     }
 
+    // takes in user name and destination
     public static void intro() {
-        // takes in user name and destination
 
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to the Trip Planner!");
@@ -20,11 +22,48 @@ public class TripPlanner {
         String destination = "";
         destination += input.nextLine();
         System.out.println("Great! " + destination + " sounds like a great trip");
+
+        System.out.println("**********");
+        System.out.println();
     }
 
+    // takes in the days planned on the trip, allowance, and conversion info
     public static void budget() {
+
         Scanner input = new Scanner(System.in);
-        // takes in the days planned on the trip, allowance, and conversion info
+        System.out.print("How many days are you going to spend travelling? ");
+        int travel_days = input.nextInt();
+        System.out.print("How much money, in EUR, are you planning to spend on your trip? ");
+        double budget = input.nextDouble();
+        System.out.print("What is the three letter currency symbol for your travel destination? ");
+        String symbol = input.next();
+        System.out.print("How many " + symbol + " are there in 1 EUR? ");
+        double conversion = input.nextDouble();
+        System.out.println();
+
+        int hours = travel_days * 24;
+        int minutes = hours * 60;
+        double daily_budget =  budget / travel_days;
+        double db_round = Math.round(daily_budget * 100) / 100.0;
+        double local_budget = (budget * conversion);
+        double daily_local_budget = local_budget / travel_days;
+        double dl_round = Math.round(daily_local_budget * 100) / 100.0;
+
+        System.out.println("If you are travelling for " + travel_days + " days that is the same as " + hours + " hours or " +
+                minutes + " minutes");
+        System.out.println("If you are going to spend €" + budget + " that means per day you can spend up to €" +
+                db_round + "EUR" );
+        System.out.println("Your total budget in " + symbol + " is " + local_budget + " " + symbol + ", which per day " +
+                "is " + dl_round + " " + symbol);
+
+        System.out.println("**********");
+        System.out.println();
+
+
+
+
+
+
 
     }
 
